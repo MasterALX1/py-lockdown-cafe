6d7e070891e764c4cd03cae192e6684ba653b4d5
Alexandre Schmid <alex@alxcloudit.com>
2025-12-31 18:18:09 -0500

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 import datetime
-from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from app.errors import NotVaccinatedError
+from app.errors import OutdatedVaccineError
+from app.errors import NotWearingMaskError
 
 
 class Cafe:
@@ -9,12 +11,21 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if not visitor.get("vaccine", False):
-            raise NotVaccinatedError(f"Visitor {visitor.get('name', 'Unknown')} is not vaccinated.")
-        
+            raise NotVaccinatedError(
+                f"Visitor {visitor.get('name', 'Unknown')}"
+                " is not vaccinated."
+            )
+
         if visitor["vaccine"].get("expiration_date") < datetime.date.today():
-            raise OutdatedVaccineError(f"Visitor {visitor.get('name', 'Unknown')}'s vaccine is outdated.")
-        
+            raise OutdatedVaccineError(
+                f"Visitor {visitor.get('name', 'Unknown')}"
+                "'s vaccine is outdated."
+            )
+
         if not visitor.get("wearing_a_mask", False):
-            raise NotWearingMaskError(f"Visitor {visitor.get('name', 'Unknown')} is not wearing a mask.")
-        
+            raise NotWearingMaskError(
+                f"Visitor {visitor.get('name', 'Unknown')}"
+                " is not wearing a mask."
+            )
+
         return f"Welcome to {self.name}"
